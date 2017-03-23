b838f6d3fe8644fb40fb85d7a2ef89c6197c6f61
Rahul Shinde <r.shinde2007@gmail.com>
2017-03-23 13:17:18 -0700

    Operations on Float values

---
+++ floats.rb
@@ -0,0 +1,11 @@
+#Float values
+
+num_one = 1.000
+
+num99 = 0.999
+
+puts num_one.to_s + " - " + num99.to_s + " = " + (num_one - num99).to_s
+
+big_float = 1.12345678901234
+
+puts (big_float + 0.0000000000005).to_s
