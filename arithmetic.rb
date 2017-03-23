d444fc203e8539795418e19c03901c95167f2bd2
Rahul Shinde <r.shinde2007@gmail.com>
2017-03-23 13:11:21 -0700

    Basic Arithmetic Operations

---
+++ arithmetic.rb
@@ -0,0 +1,17 @@
+# Arithmetic Operations
+
+print "Enter a Value:"
+
+first_num = gets.to_i
+
+print "Enter Second Value:"
+
+second_num = gets.to_i
+
+puts first_num.to_s + " + " + second_num.to_s + " = " + (first_num + second_num).to_s
+
+puts first_num.to_s + " - " + second_num.to_s + " = " + (first_num - second_num).to_s
+
+puts first_num.to_s + " * " + second_num.to_s + " = " + (first_num * second_num).to_s
+
+puts first_num.to_s + " / " + second_num.to_s + " = " + (first_num / second_num).to_s
