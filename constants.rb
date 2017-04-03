fb2d6022c65428086f8ddf21b9cbb58d65683d21
Rahul Shinde <r.shinde2007@gmail.com>
2017-04-02 22:19:31 -0700

    Removed Duplicate Files

--- constants.rb
+++
@@ -1,7 +0,0 @@
-# Initialization of Constants and changing value of it
-
-A_CONSTANT = 3.14
-
-A_CONSTANT = 1.6
-
-puts A_CONSTANT
--- object.rb
+++
@@ -1,6 +0,0 @@
-puts 1.class
-
-puts 1.234.class
-
-puts "A String".class
-
